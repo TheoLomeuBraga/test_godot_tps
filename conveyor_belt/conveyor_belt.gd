13babf5befbc294563bdd5d31345708caa7e5a07
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-03 18:55:01 -0300

    a

--- conveyor_belt/conveyor_belt.gd
+++ conveyor_belt/conveyor_belt.gd
@@ -2,13 +2,24 @@ extends Node
 class_name ConveyorBeltManager
 
 @export var conveyor_belt_model : Mesh
-@export var items_models : Dictionary[String,Mesh]
+@onready var coveyer_belt_mult_mesh : MultiMeshInstance3D
 
+@export var items_models : Dictionary[String,Mesh]
 @export var conveyor_belts : Dictionary[Vector3i,ConveyorBeltData]
 
+func start_tests() -> void:
+	coveyer_belt_mult_mesh.multimesh.set_instance_transform(0, Transform3D(Basis(), Vector3(0, 0, 0)))
 
 func _ready() -> void:
-	pass
+	coveyer_belt_mult_mesh = MultiMeshInstance3D.new()
+	coveyer_belt_mult_mesh.multimesh = MultiMesh.new()
+	coveyer_belt_mult_mesh.multimesh.transform_format = MultiMesh.TransformFormat.TRANSFORM_3D
+	coveyer_belt_mult_mesh.multimesh.mesh = conveyor_belt_model
+	coveyer_belt_mult_mesh.multimesh.instance_count = 100
+	add_child(coveyer_belt_mult_mesh)
+	
+	start_tests()
+
 
 
 func _process(delta: float) -> void:
--- conveyor_belt/conveyor_belt_data.gd
+++ conveyor_belt/conveyor_belt_data.gd
@@ -4,3 +4,4 @@ class_name ConveyorBeltData
 @export var next : ConveyorBeltData
 @export var items : ConveyorBeltItem
 @export var speed : float = 1.0
+@export var end_position : Vector3
