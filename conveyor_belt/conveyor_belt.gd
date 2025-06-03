811eb145bae2cc7bfd7399edd5ef924daf53b3fc
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-03 19:33:11 -0300

    a

--- conveyor_belt/conveyor_belt.gd
+++ conveyor_belt/conveyor_belt.gd
@@ -5,18 +5,27 @@ class_name ConveyorBeltManager
 @onready var coveyer_belt_mult_mesh : MultiMeshInstance3D
 
 @export var items_models : Dictionary[String,Mesh]
-@export var conveyor_belts : Dictionary[Vector3i,ConveyorBeltData]
+var items_mult_mesh_models : Dictionary[String,MultiMeshInstance3D]
+var conveyor_belts : Dictionary[Vector3i,ConveyorBeltData]
 
 func start_tests() -> void:
 	coveyer_belt_mult_mesh.multimesh.set_instance_transform(0, Transform3D(Basis(), Vector3(0, 0, 0)))
+	items_mult_mesh_models["cube"].multimesh.set_instance_transform(0, Transform3D(Basis(), Vector3(0, 0, 0)))
+
+func initiate_mult_mesh_instance(mesh : Mesh) -> MultiMeshInstance3D:
+	var mult_mesh : MultiMeshInstance3D = MultiMeshInstance3D.new()
+	mult_mesh.multimesh = MultiMesh.new()
+	mult_mesh.multimesh.transform_format = MultiMesh.TransformFormat.TRANSFORM_3D
+	mult_mesh.multimesh.mesh = mesh
+	mult_mesh.multimesh.instance_count = 1000
+	add_child(mult_mesh)
+	return mult_mesh
 
 func _ready() -> void:
-	coveyer_belt_mult_mesh = MultiMeshInstance3D.new()
-	coveyer_belt_mult_mesh.multimesh = MultiMesh.new()
-	coveyer_belt_mult_mesh.multimesh.transform_format = MultiMesh.TransformFormat.TRANSFORM_3D
-	coveyer_belt_mult_mesh.multimesh.mesh = conveyor_belt_model
-	coveyer_belt_mult_mesh.multimesh.instance_count = 100
-	add_child(coveyer_belt_mult_mesh)
+	coveyer_belt_mult_mesh = initiate_mult_mesh_instance(conveyor_belt_model)
+	for name in items_models:
+		items_mult_mesh_models[name] = initiate_mult_mesh_instance(items_models[name])
+		print("add: ",name)
 	
 	start_tests()
 
