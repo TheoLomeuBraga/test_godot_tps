3ce8b34207c3845cd3c890423ab47c399ebae1ac
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-04 15:52:17 -0300

    a

--- conveyor_belt/conveyor_belt.gd
+++ conveyor_belt/conveyor_belt.gd
@@ -11,7 +11,7 @@ var conveyor_belts : Dictionary[Vector3i,ConveyorBeltData]
 var update_belts_next : bool = false
 var clean_items : bool = true
 
-const max_item_per_type_count = 1000
+const max_item_per_type_count = 100000
 func initiate_mult_mesh_instance(mesh : Mesh) -> MultiMeshInstance3D:
 	var mult_mesh : MultiMeshInstance3D = MultiMeshInstance3D.new()
 	mult_mesh.multimesh = MultiMesh.new()
@@ -35,7 +35,12 @@ func update_cb_next_target(position : Vector3i) -> void:
 		else:
 			belt.next = null
 
+var belt_count : int = 0
+
+var belts_array : Array[ConveyorBeltData]
+
 func add_belt(position : Vector3i, rotation : Vector3i = Vector3.ZERO,speed : float = 1.0) -> void:
+	
 	var belt : ConveyorBeltData = ConveyorBeltData.new()
 	add_child(belt)
 	belt.global_position = position
@@ -52,6 +57,17 @@ func add_belt(position : Vector3i, rotation : Vector3i = Vector3.ZERO,speed : fl
 	update_cb_next_target(position + to_vec3i(Vector3(0.0,-1.0,0.0)))
 	
 	update_belts_next = true
+	
+	belts_array.push_back(belt)
+	
+	belt_count += 1
+	
+
+func belt_has_valid_item(belt_position : Vector3i) -> bool:
+	for i in conveyor_belts[belt_position].items.size():
+		if conveyor_belts[belt_position].items[i].valid:
+			return true
+	return false
 
 func add_item_to_belt(belt_position : Vector3i,item_name : String,item_position : Vector3 = belt_position) -> void:
 	var item : ConveyorBeltItem = ConveyorBeltItem.new()
@@ -95,14 +111,42 @@ func create_simple_test_vertical_loop_belt(pos : Vector3i = Vector3.ZERO) -> voi
 	
 	add_item_to_belt(pos,"cube")
 	add_item_to_belt(to_vec3i(Vector3(pos) + Vector3(1,-1,0)),"sphere")
+
+func create_simple_test_mega_belt(pos : Vector3i = Vector3.ZERO) -> void:
+	add_belt(pos,Vector3(0,0,0),20)
+	add_belt(to_vec3i(Vector3(pos) + Vector3(1,0,0)),to_vec3i(Vector3(0,90,0)),20)
 	
+	var belt_speed : float = 10
+	
+	for i in range(1,100):
+		add_belt(pos + Vector3i(0,0,-i), Vector3(0,0,0) ,belt_speed)
+		add_belt(pos + Vector3i(1,0,-i), Vector3(0,180,0) ,belt_speed)
+		
+	
+	for i in range(10,90):
+		if i % 2 == 1:
+			add_item_to_belt(pos + Vector3i(0,0,-i),"cube")
+			add_item_to_belt(pos + Vector3i(1,0,-i),"cube")
+		else:
+			add_item_to_belt(pos + Vector3i(0,0,-i),"sphere")
+			add_item_to_belt(pos + Vector3i(1,0,-i),"sphere")
+	
+	add_belt(to_vec3i(Vector3(pos) + Vector3(0,0,-100)),to_vec3i(Vector3(0,-90,0)),belt_speed)
+	add_belt(to_vec3i(Vector3(pos) + Vector3(1,0,-100)),to_vec3i(Vector3(0,180,0)),belt_speed)
+	
+	#add_item_to_belt(pos,"cube")
+	#add_item_to_belt(to_vec3i(Vector3(pos) + Vector3(1,0,-1)),"sphere")
 
 func start_tests() -> void:
 	create_simple_test_belt()
 	create_simple_test_loop_belt(to_vec3i(Vector3(10,0,0)))
 	create_simple_test_vertical_loop_belt(to_vec3i(Vector3(20,0,0)))
 	
+	for i in range(0,20):
+		create_simple_test_mega_belt(to_vec3i(Vector3(30 + (i * 2),0,0)))
 	
+	print("belt_count: ",belt_count)
+
 
 var transform_null : Transform3D
 
@@ -125,7 +169,9 @@ func process_tests(delta: float) -> void:
 				clean_items = true
 
 
-func _process(delta: float) -> void:
+
+func process_belts(delta: float = 60.0/1.0) -> void:
+	#print("process_belts")
 	if update_belts_next:
 		var i : int = 0
 		for c in get_children():
@@ -137,36 +183,44 @@ func _process(delta: float) -> void:
 	if clean_items:
 		for mm in items_mult_mesh_models:
 			for i in range(0,max_item_per_type_count):
+				if items_mult_mesh_models[mm].multimesh.get_instance_transform(i) == transform_null:
+					break
 				items_mult_mesh_models[mm].multimesh.set_instance_transform(i,transform_null)
 		clean_items = false
 	
-	#update_visual_positions
+	
 	var item_progresion : Dictionary[String,int]
 	for mm in items_mult_mesh_models:
 		
 		item_progresion[mm] = 0
 	
-	for c in get_children():
-		if c is ConveyorBeltData:
+	for id in belts_array.size():
+		
+		var c : ConveyorBeltData = belts_array[id]
+		
+		var cb_end = c.to_global(Vector3(0.0,0.0,-0.5))
 			
-			var cb_end = c.to_global(Vector3(0.0,0.0,-0.5))
-			
-			for i : int in range(0,c.items.size()):
+		for i : int in range(0,c.items.size()):
 				
-				var i_data : ConveyorBeltItem = c.items[i]
+			var i_data : ConveyorBeltItem = c.items[i]
 				
-				if i_data.valid:
+			if i_data.valid:
 					
-					items_mult_mesh_models[i_data.item_name].multimesh.set_instance_transform(item_progresion[i_data.item_name],Transform3D(Basis(),i_data.position))
-					item_progresion[i_data.item_name] += 1
+				items_mult_mesh_models[i_data.item_name].multimesh.set_instance_transform(item_progresion[i_data.item_name],Transform3D(Basis(),i_data.position))
+				item_progresion[i_data.item_name] += 1
 					
 					
 					
-					i_data.position = i_data.position.move_toward(cb_end,c.speed * delta)
-					if i_data.position == cb_end and c.next != null:
-						i_data.valid = false
-						add_item_to_belt(to_vec3i(c.next.position),i_data.item_name,i_data.position)
-						
-						clean_items = true
+				i_data.position = i_data.position.move_toward(cb_end,c.speed * delta)
+				if i_data.position == cb_end and c.next != null and not belt_has_valid_item(to_vec3i(c.next.position)):
+					i_data.valid = false
+					add_item_to_belt(to_vec3i(c.next.position),i_data.item_name,i_data.position)
+					
+					clean_items = true
 						
 	process_tests(delta)
+
+
+func _process(delta: float) -> void:
+	
+	process_belts(delta)
