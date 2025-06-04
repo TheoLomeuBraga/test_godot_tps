4a3ca915ebceaf4c9bef7d69a0ca99918d91b851
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-04 19:13:15 -0300

    a

--- conveyor_belt/conveyor_belt.gd
+++ conveyor_belt/conveyor_belt.gd
@@ -29,9 +29,16 @@ func to_vec3i(v : Vector3) -> Vector3i:
 func update_cb_next_target(position : Vector3i) -> void:
 	if conveyor_belts.has(position):
 		var belt : ConveyorBeltData = conveyor_belts[position]
-		var next_belt_pos : Vector3i = to_vec3i(belt.to_global(Vector3(0.0,0.0,-1.0)))
+		
+		belt_tf_calculator.global_position = belt.position
+		belt_tf_calculator.rotation_degrees = belt.rotation
+		
+		var next_belt_pos : Vector3i = to_vec3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-1.0)))
+		if Vector3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-1.0))) != to_vec3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-1.0))):
+			print(Vector3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-1.0))),to_vec3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-1.0))))
+		
 		if conveyor_belts.has(next_belt_pos):
-			belt.next = conveyor_belts[next_belt_pos]
+			belt.next = conveyor_belts[next_belt_pos] # <-
 		else:
 			belt.next = null
 
@@ -39,13 +46,24 @@ var belt_count : int = 0
 
 var belts_array : Array[ConveyorBeltData]
 
+var belt_tf_calculator : Node3D
+
 func add_belt(position : Vector3i, rotation : Vector3i = Vector3.ZERO,speed : float = 1.0) -> void:
 	
 	var belt : ConveyorBeltData = ConveyorBeltData.new()
-	add_child(belt)
-	belt.global_position = position
-	belt.global_rotation_degrees = rotation
+	
+
+	
+	belt.position = position
+	belt.rotation = rotation
 	belt.speed = speed
+	
+	belt_tf_calculator.global_position = position
+	belt_tf_calculator.global_rotation_degrees = rotation
+	
+	belt.transform = belt_tf_calculator.transform
+	
+	belt.end = to_vec3i(belt_tf_calculator.to_global(Vector3(0.0,0.0,-0.5)))
 	conveyor_belts[position] = belt
 	
 	update_cb_next_target(position)
@@ -93,8 +111,8 @@ func create_simple_test_belt(pos : Vector3i = Vector3.ZERO) -> void:
 
 func create_simple_test_loop_belt(pos : Vector3i = Vector3.ZERO) -> void:
 	
-	add_belt(pos)
-	add_belt(to_vec3i(Vector3(pos) + Vector3(1,0,0)),to_vec3i(Vector3(0,90,0))) # <-
+	add_belt(pos,to_vec3i(Vector3(0,0,0)))
+	add_belt(to_vec3i(Vector3(pos) + Vector3(1,0,0)),to_vec3i(Vector3(0,90,0)))
 	add_belt(to_vec3i(Vector3(pos) + Vector3(0,0,-1)),to_vec3i(Vector3(0,-90,0)))
 	add_belt(to_vec3i(Vector3(pos) + Vector3(1,0,-1)),to_vec3i(Vector3(0,180,0)))
 	
@@ -138,19 +156,23 @@ func create_simple_test_mega_belt(pos : Vector3i = Vector3.ZERO) -> void:
 	#add_item_to_belt(to_vec3i(Vector3(pos) + Vector3(1,0,-1)),"sphere")
 
 func start_tests() -> void:
+	
 	create_simple_test_belt()
 	create_simple_test_loop_belt(to_vec3i(Vector3(10,0,0)))
 	create_simple_test_vertical_loop_belt(to_vec3i(Vector3(20,0,0)))
 	
+	
 	for i in range(0,20):
 		create_simple_test_mega_belt(to_vec3i(Vector3(30 + (i * 2),0,0)))
 	
-	print("belt_count: ",belt_count)
 
 
 var transform_null : Transform3D
 
 func _ready() -> void:
+	
+	belt_tf_calculator = Node3D.new()
+	add_child(belt_tf_calculator)
 	
 	transform_null = Transform3D(Basis(),Vector3.ZERO).scaled(Vector3.ZERO)
 	
@@ -170,15 +192,40 @@ func process_tests(delta: float) -> void:
 
 
 
-func process_belts(delta: float = 60.0/1.0) -> void:
-	#print("process_belts")
+var global_delta : float = 1.0/60.0
+
+var aAa : float = 0.0
+
+func process_single_belt(id : int) -> void:
+	
+	var c : ConveyorBeltData = belts_array[id]
+	
+			
+	for i : int in range(0,c.items.size()):
+				
+		var i_data : ConveyorBeltItem = c.items[i]
+				
+		if i_data != null and i_data.valid:
+			
+			i_data.position = i_data.position.move_toward(c.end,c.speed * global_delta)
+			if i_data.position == c.end and c.next != null and not belt_has_valid_item(to_vec3i(c.next.position)):
+				
+				#if aAa > 5:
+				#	print(c.position," ",c.next.position)
+				
+				i_data.valid = false
+				add_item_to_belt(to_vec3i(c.next.position),i_data.item_name,i_data.position)
+					
+				clean_items = true
+
+func update_belt_visual() -> void:
 	if update_belts_next:
 		var i : int = 0
-		for c in get_children():
-			if c is ConveyorBeltData:
-				coveyer_belt_mult_mesh.multimesh.set_instance_transform(i, c.transform)
-				update_belts_next = false
-				i+=1
+		for c in belts_array:
+			
+			coveyer_belt_mult_mesh.multimesh.set_instance_transform(i, c.transform)
+			update_belts_next = false
+			i+=1
 	
 	if clean_items:
 		for mm in items_mult_mesh_models:
@@ -191,36 +238,38 @@ func process_belts(delta: float = 60.0/1.0) -> void:
 	
 	var item_progresion : Dictionary[String,int]
 	for mm in items_mult_mesh_models:
-		
 		item_progresion[mm] = 0
 	
 	for id in belts_array.size():
-		
 		var c : ConveyorBeltData = belts_array[id]
-		
-		var cb_end = c.to_global(Vector3(0.0,0.0,-0.5))
-			
-		for i : int in range(0,c.items.size()):
-				
+		for i : int in c.items.size():
 			var i_data : ConveyorBeltItem = c.items[i]
-				
 			if i_data.valid:
-					
 				items_mult_mesh_models[i_data.item_name].multimesh.set_instance_transform(item_progresion[i_data.item_name],Transform3D(Basis(),i_data.position))
 				item_progresion[i_data.item_name] += 1
-					
-					
-					
-				i_data.position = i_data.position.move_toward(cb_end,c.speed * delta)
-				if i_data.position == cb_end and c.next != null and not belt_has_valid_item(to_vec3i(c.next.position)):
-					i_data.valid = false
-					add_item_to_belt(to_vec3i(c.next.position),i_data.item_name,i_data.position)
-					
-					clean_items = true
-						
+	
+	
+
+var belt_task_id : int = -1
+
+func process_belts(delta: float) -> void:
+	
+	
+	#for id in belts_array.size():
+	#	process_single_belt(id)
+	
+	if belt_task_id == -1:
+		belt_task_id = WorkerThreadPool.add_group_task(process_single_belt, belts_array.size())
+	
+	if WorkerThreadPool.is_group_task_completed(belt_task_id):
+		update_belt_visual()
+		belt_task_id = WorkerThreadPool.add_group_task(process_single_belt, belts_array.size())
+		
+	
+	
 	process_tests(delta)
 
 
 func _process(delta: float) -> void:
-	
+	aAa += delta
 	process_belts(delta)
--- conveyor_belt/conveyor_belt_data.gd
+++ conveyor_belt/conveyor_belt_data.gd
@@ -1,6 +1,10 @@
-extends Node3D
+extends Resource
 class_name ConveyorBeltData
 
 @export var next : ConveyorBeltData
 @export var items : Array[ConveyorBeltItem]
 @export var speed : float = 1.0
+@export var position : Vector3i
+@export var rotation : Vector3i
+@export var end : Vector3
+@export var transform : Transform3D
