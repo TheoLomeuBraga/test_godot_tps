f5fb83870df39f852616d063aa43131dd0c69200
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-12 11:37:53 -0300

    a

---
+++ projector/projector.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+func _process(delta: float) -> void:
+	pass
+	#var image_texture: ImageTexture = ImageTexture.new()
+	#$SpotLight3D.light_projector = image_texture.create_from_image($MeshInstance3D2/SubViewport.get_texture())
