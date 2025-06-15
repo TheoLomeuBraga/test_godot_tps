4e4178ce33b1589f227788254fc3152087a1199c
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-15 16:55:37 -0300

    a

--- TestRagdolMetaRig/physical_bone_simulator_3d.gd
+++ TestRagdolMetaRig/physical_bone_simulator_3d.gd
@@ -1,5 +1,7 @@
 extends PhysicalBoneSimulator3D
 
 func _ready() -> void:
+	for c in get_children():
+		if c is PhysicalBone3D:
+			c.joint_type = 5
 	physical_bones_start_simulation()
-	print(is_simulating_physics())
