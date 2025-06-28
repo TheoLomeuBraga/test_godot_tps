78c0ebb5349179c4b795502b13f6fd4f875bff3b
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-06-28 19:11:48 -0300

    a

--- build_coveyor_belt_system/build_system.gd
+++ build_coveyor_belt_system/build_system.gd
@@ -3,6 +3,13 @@ extends Node3D
 @onready var camera : Camera3D = $Camera3D
 @onready var indicator : Node3D = $indicator
 
+var current_point_index : int = 0
+
 func _physics_process(delta: float) -> void:
 	indicator.global_position = camera.project_position(get_viewport().get_mouse_position(), camera.global_transform.origin.z)
-	indicator.global_position.y = 0
+	indicator.global_position.y = 1
+	
+	
+	if Input.is_action_just_pressed("shot"):
+		$belt/Path3D.curve.add_point(indicator.global_position,Vector3.ZERO,Vector3.ZERO)
+		current_point_index += 1
