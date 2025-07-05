e0b88d90426f228df626433c642d222cede09ad1
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-07-05 08:40:27 -0300

    a

--- linked_nav_mesh/link_agent.gd
+++ linked_nav_mesh/link_agent.gd
@@ -6,13 +6,13 @@ extends CharacterBody3D
 var on_link : bool = false
 var target_position : Vector3
 
+var speed : float = 10.0
+
 func _physics_process(delta: float) -> void:
 	
 	if not on_link:
 		$NavigationAgent3D.target_position = target.global_position
 	velocity = ($NavigationAgent3D.get_next_path_position() - global_position).normalized() * 5.0
-	if not on_link:
-		velocity.y -= delta * 9.0
 	move_and_slide()
 	
 	
