95172173db6c400c7ddd9dfb75714af4c83a14ef
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-07-04 21:18:40 -0300

    a

--- navmesh_test/agent/test_agent.gd
+++ navmesh_test/agent/test_agent.gd
@@ -5,23 +5,29 @@ class_name TestAgent
 
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
-var node_target : Node3D
+@export var node_target : Node3D
 func _ready() -> void:
 	$MeshInstance3D.mesh.material.r = rng.randf_range(0,1)
 	$MeshInstance3D.mesh.material.g = rng.randf_range(0,1)
 	$MeshInstance3D.mesh.material.r = rng.randf_range(0,1)
 	
-	node_target = get_parent()
+	if node_target == null:
+		node_target = get_parent()
 
 
 var move_direction : Vector3
 
 
 func recalculate_path():
-	$RecalculatePathTimer.wait_time = rng.randf_range(0.25,1.25)
+	$RecalculatePathTimer.wait_time = rng.randf_range(0.05,0.2)
 	$NavigationAgent3D.target_position = node_target.global_position
+	$NavigationAgent3D.target_position.y += 0.5
 	var target_position : Vector3 = $NavigationAgent3D.get_next_path_position()
-	move_direction = (target_position - global_position).normalized()
+	move_direction = (target_position - global_position)
+	#move_direction.y += 1
+	move_direction = move_direction.normalized()
+	
+	print(move_direction.y)
 
 func _physics_process(delta: float) -> void:
 	velocity = move_direction * speed
