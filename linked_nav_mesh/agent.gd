e17fe3cfacc1a387831a8c549a2df691b3bbf9a5
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-07-04 23:49:55 -0300

    a

--- linked_nav_mesh/agent.gd
+++ linked_nav_mesh/agent.gd
@@ -1,5 +1,4 @@
 extends CharacterBody3D
-class_name TestAgent
 
 @export var speed = 500.0
 
@@ -7,6 +6,7 @@ var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 @export var node_target : Node3D
 func _ready() -> void:
+	recalculate_path()
 	$MeshInstance3D.mesh.material.r = rng.randf_range(0,1)
 	$MeshInstance3D.mesh.material.g = rng.randf_range(0,1)
 	$MeshInstance3D.mesh.material.r = rng.randf_range(0,1)
@@ -17,29 +17,26 @@ func _ready() -> void:
 
 var move_direction : Vector3
 
-
+var nav_querie : NavigationPathQueryResult3D
+var nav_querie_pos : int = 0
 func recalculate_path():
-	$RecalculatePathTimer.wait_time = rng.randf_range(0.05,0.2)
 	$NavigationAgent3D.target_position = node_target.global_position
-	$NavigationAgent3D.target_position.y += 0.5
-	var target_position : Vector3 = $NavigationAgent3D.get_next_path_position()
-	move_direction = (target_position - global_position)
-	#move_direction.y += 1
-	move_direction = move_direction.normalized()
-	
-	print(move_direction.y)
+	nav_querie = $NavigationAgent3D.get_current_navigation_result()
+	print($NavigationAgent3D.target_position)
+
+func next_path_point() -> void:
+	nav_querie_pos += 1
 
 func _physics_process(delta: float) -> void:
-	velocity = move_direction * speed
-	move_and_slide()
 	
-	#global_position += move_direction * speed * delta
+	if nav_querie.path.size() <= nav_querie_pos:
+		recalculate_path()
+		return
 	
-	$NavigationAgent3D.set_velocity_forced(move_direction * speed * delta)
+	global_position.move_toward(nav_querie.path[nav_querie_pos],speed * delta)
+	global_position.move_toward($NavigationAgent3D.get_next_path_position(),speed * delta)
+
 
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
-	velocity = safe_velocity
-	move_and_slide()
-	
-	#global_position += safe_velocity
+	pass
---
+++ linked_nav_mesh/link_agent.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+@export var target : Node3D
+
+var on_link : bool = false
+var target_position : Vector3
+
+func _physics_process(delta: float) -> void:
+	
+	if not on_link:
+		$NavigationAgent3D.target_position = target.global_position
+	velocity = ($NavigationAgent3D.get_next_path_position() - global_position).normalized() * 5.0
+	if not on_link:
+		velocity.y -= delta * 9.0
+	move_and_slide()
+	
+	
+
+
+func _on_navigation_agent_3d_link_reached(details: Dictionary) -> void:
+	on_link = true
+	target_position = details["link_exit_position"]
+
+
+func _on_navigation_agent_3d_target_reached() -> void:
+	on_link = false
