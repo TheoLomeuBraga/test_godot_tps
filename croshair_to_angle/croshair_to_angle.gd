1f86db6c4f6283483b08ef60511b0791eecc3300
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-08-03 14:19:31 -0300

    a

---
+++ croshair_to_angle/croshair_to_angle.gd
@@ -0,0 +1,40 @@
+extends Camera3D
+class_name CroshairToAngle
+
+@export_range(0.0,2.0,0.1) var dispersion : float = 1.0
+@export var angle_calculation_target_path : NodePath
+var angle_calculation_target : Node3D
+
+func play_test() -> void:
+	$AnimationPlayer.play("test_cross")
+
+func _ready() -> void:
+	angle_calculation_target = get_node(angle_calculation_target_path)
+	for c in get_children():
+		if c is Node3D:
+			var n : Node3D = c
+			n.set_meta("rest_position",n.position)
+	
+	#play_test()
+	
+
+func calculate_angle() -> float:
+	
+	var screen_size : float = DisplayServer.window_get_size().x / 2
+	
+	var screen_pos : float = unproject_position(angle_calculation_target.position).x
+	screen_pos -= screen_size
+	
+	var progress : float = lerp(0.0,1.0,screen_pos/screen_size)
+	
+	return lerp(0.0,fov,progress)
+
+func _process(delta: float) -> void:
+	for c in get_children():
+		if c is Node3D:
+			var n : Node3D = c
+			n.position = n.get_meta("rest_position") * dispersion
+			n.position.z = n.get_meta("rest_position").z
+			
+	
+	print("angle calculated: ",calculate_angle())
