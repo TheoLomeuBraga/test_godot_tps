bed3e521f1926de7091efcbbea5a138fbda45d88
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-08-12 17:41:10 -0300

    a

--- trail_fx/trail_fx.gd
+++ trail_fx/trail_fx.gd
@@ -3,39 +3,41 @@ class_name TrailFx
 
 var parent : Node3D
 var camera : Camera3D
+var point_life_time : float
+
+@export var trail_time_lenght : float = 0.5
 
 func _ready() -> void:
+	point_life_time = trail_time_lenght
 	parent = get_parent()
 	camera = get_viewport().get_camera_3d()
 	clear_points()
 
-@export var trail_point_duration : float = 1.0
-@export var time_to_add_point : float = 0.1
-var life_time_per_point : Array[float]
-var points_3D : Array[Vector4]
 
-var time_to_next_point : float = 0
+var points_3D : Array[Vector3]
+var time_passed : float = 0
+
 func _process(delta: float) -> void:
+	
 	clear_points()
-	if visible:
-		time_to_next_point -= delta
-		if time_to_next_point < 0:
-			points_3D.push_back(Vector4(parent.global_position.x,parent.global_position.y,parent.global_position.z,trail_point_duration))
-		
-		for i in range(0,points_3D.size()):
-			points_3D[i].w -= delta
-		
-		for i in range(0,points_3D.size()):
-			if points_3D[i].w < 0:
-				points_3D.remove_at(i)
-				break
+	
+	point_life_time -= delta
+	
+	points_3D.push_back(Vector3(parent.global_position.x,parent.global_position.y,parent.global_position.z))
+	
+	if point_life_time < 0:
+		points_3D.remove_at(0)
 	
 	for i in range(0,points_3D.size()):
-		var pos : Vector3 = Vector3(points_3D[i].x,points_3D[i].y,points_3D[i].z)
+		var pos : Vector3 = points_3D[i]
 		if camera.is_position_behind(pos):
-			break;
+			if i == points_3D.size()-1:
+				return
+			else:
+				continue
 		
 		add_point(camera.unproject_position(pos))
-		add_point(camera.unproject_position(parent.global_position))
+	
+	add_point(camera.unproject_position(parent.global_position))
 	
 	
